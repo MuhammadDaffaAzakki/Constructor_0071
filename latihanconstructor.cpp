6d43a02f1c36459dc7d98f54a265cd04ecf6c531
MuhammadDaffaAzakki <daffaazakki29@gmail.com>
2025-05-20 15:41:50 +0700

    Latihan constructor berparameter

---
+++ latihanconstructor.cpp
@@ -0,0 +1,24 @@
+// Buatlah program c++ dengan class barang yang memiliki atribut: namaBarang, KodeBarang
+// Program memiliki constructor berparameter
+// untuk mengisi nilai namaBarang dan kodeBarang
+// serta memiliki fungsi untuk menampilkan informasi barang
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class barang {
+    public:
+        barang(string namaBarang, int kodeBarang);
+};
+
+barang::barang (string namaBarang, int kodeBarang) {
+    cout << " Constructor dengan parameter terpanggil" << endl;
+    cout << "Nama : " << namaBarang << endl;
+    cout << "Kode : " << kodeBarang << endl;
+}
+
+int main() {
+    barang brg("Laptop", 292929);
+    return 0;
+}
